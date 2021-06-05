9816ed0992e78a2f930715e9bd1fe771901e445e
Maryam <mariam.babayeva@gmail.com>
2021-06-05 20:14:18 +0400

    feat: added function for creating the board

---
+++ board.c
@@ -0,0 +1,27 @@
+#include "board.h"
+#include <string.h>
+
+Board *Board_create(int width, int height)
+{
+    Board *board = malloc(sizeof(Board));
+    memset(board, 0, sizeof(*board));
+
+    board->width = width;
+    board->height = height;
+    board->grid = (bool **)malloc(board->height * (sizeof(bool *)));
+
+    for (int i = 0; i < board->height; i++)
+    {
+        board->grid[i] = (bool *)malloc(board->width * sizeof(bool));
+    }
+
+    for (int i = 0; i < board->height; i++)
+    {
+        for (int j = 0; j < board->width; j++)
+        {
+            board->grid[i][j] = 0;
+        }
+    }
+
+    return board;
+}
