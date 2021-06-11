2fdb5a9d3e46d653628504d3f0b41cda34d1776a
mbabaeva <mariam.babayeva@gmail.com>
2021-06-11 19:10:29 +0400

    [test-board] Add blinker pattern test

--- tests/test.c
+++ tests/test.c
@@ -1,13 +1,33 @@
 #include <CUnit/Basic.h>
 
 #include <unistd.h>
+#include "../liblife/board.h"
 
 static int init_suite() { return 0; }
 
 static int clean_suite() { return 0; }
 
-void test_dummy() {
-  CU_ASSERT(1);
+/**
+ * @brief Test blinker pattern
+ *
+ * https://cs.stanford.edu/people/eroberts/courses/soco/projects/2001-02/cellular-automata/oscillators/blinker.gif
+ */
+void test_blinker() {
+  Board *result = Board_create(3, 3);
+
+  result->grid[0][1] = true;
+  result->grid[1][1] = true;
+  result->grid[2][1] = true;
+
+  Board *start = Board_create(3, 3);
+
+  start->grid[1][0] = true;
+  start->grid[1][1] = true;
+  start->grid[1][2] = true;
+
+  start = Board_make_life(start, true);
+
+  CU_ASSERT(Board_compare(start, result));
 }
 
 int main() {
@@ -15,14 +35,14 @@ int main() {
     return CU_get_error();
   }
 
-  CU_pSuite suite = CU_add_suite("Suite_1", init_suite, clean_suite);
+  CU_pSuite suite = CU_add_suite("Patterns", init_suite, clean_suite);
 
   if (!suite) {
     CU_cleanup_registry();
     return CU_get_error();
   }
 
-  if ((NULL == CU_add_test(suite, "Dummy", test_dummy))) {
+  if ((NULL == CU_add_test(suite, "Blinker", test_blinker))) {
     CU_cleanup_registry();
     return CU_get_error();
   }
